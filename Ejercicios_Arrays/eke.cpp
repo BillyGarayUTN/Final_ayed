bf5413190df6cf2809fa464978897356fde58bee
BillyGarayUTN <bgarayquispe@frba.utn.edu.ar>
2025-08-08 03:44:46 -0300

    se subieron lso cambios 2

---
+++ Ejercicios_Arrays/eke.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+/* 
+Ejercicio 1 
+Dados dos valores numéricos enteros a y b, calcular e informar
+la suma: a+b, la diferencia: a-b, y el producto: 
+a*b entre dichos valores.
+*/
+
+
+int main() {
+    
+    int a,b;
+    cout << "Ingrese el primer valor entero: ";
+    cin >> a;
+    cout << "Ingrese el segundo valor entero: ";
+    cin >> b;
+
+    cout << "La suma es: " << a + b << endl;
+    cout << "La diferencia es: " << a - b << endl;
+    cout << "El producto es: " << a * b << endl;
+
+    return 0;
+}
